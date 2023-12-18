c563dd046c40fe14d1412028a9012f08b560ac40
Kogoon <kogoon@mz.co.kr>
2023-12-18 04:54:43 +0000

    Added S3 bucket module

--- main.tf
+++ main.tf
@@ -11,6 +11,21 @@ provider "aws" {
   region  = var.region
 }
 
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+
+  bucket = "xxkogoon-s3-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
+
 resource "aws_vpc" "hashicat" {
   cidr_block           = var.address_space
   enable_dns_hostnames = true
